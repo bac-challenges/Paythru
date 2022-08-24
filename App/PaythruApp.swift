acc1dbd9ab062840d1e79e3812c31ccfd070c0d3
emile <emile@blueantcorp.com>
2022-08-24 18:17:12 +0400

    init

---
+++ App/PaythruApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaythruApp.swift
+//  Paythru
+//
+//  Created by emile on 24/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PaythruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Paythru
+
+## Overview
+Find likely country base by name
+
+## General
+- Swift 5.6
+- iOS 15
+- Xcode 14
+- SwiftUI
+- Combine
